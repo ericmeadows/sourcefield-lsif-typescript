c4c76ba378b5f86d4db9210627486d9c9cc89fd6
Eric Meadows <eric@plumbr.ai>
2022-10-26 10:47:04 -0400

    Fixed inferTsconfig function

--- src/inferTsconfig.ts
+++ src/inferTsconfig.ts
@@ -1,17 +1,17 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from 'fs';
+import * as path from 'path';
 
 /**
  * To limit the risk of making the `inferTsconfig` run for a very long time, we
  * stop the file traversal after visiting this number of files.
  */
-const maximumFileTraversalCount = 1_000
+const maximumFileTraversalCount = 1_000;
 
 /** The TS config we use to index JavaScript files. */
-export const allowJsConfig = '{"compilerOptions":{"allowJs":true}}'
+export const allowJsConfig = '{"exclude": ["**/node_modules"], "compilerOptions":{"allowJs":true}}';
 
 /** The TS config we use to index only TypeScript files. */
-export const noJsConfig = '{}'
+export const noJsConfig = '{"exclude": ["**/node_modules"]}';
 
 /**
  * Returns the configuration that should be used for tsconfig.json in the provided path.
@@ -20,36 +20,37 @@ export const noJsConfig = '{}'
  * If the directory doesn't contains one `*.{ts,tsx}` file then the config will
  */
 export function inferTsconfig(projectPath: string): string {
-  let hasTypeScriptFile = false
-  let hasJavaScriptFile = false
-  let visitedFileCount = 0
-  const visitPath = (directory: string): { stop: boolean } => {
-    if (directory.endsWith('.ts') || directory.endsWith('.tsx')) {
-      hasTypeScriptFile = true
-      return { stop: true }
-    }
-    if (directory.endsWith('.js') || directory.endsWith('.jsx')) {
-      hasJavaScriptFile = true
-    }
-    if (!fs.statSync(directory).isDirectory()) {
-      return { stop: false }
-    }
-    for (const child of fs.readdirSync(directory)) {
-      visitedFileCount++
-      if (visitedFileCount > maximumFileTraversalCount) {
-        return { stop: true }
-      }
-      const fullPath = path.resolve(directory, child)
-      const recursiveWalk = visitPath(fullPath)
-      if (recursiveWalk.stop) {
-        return recursiveWalk
-      }
-    }
-    return { stop: false }
-  }
-  visitPath(projectPath)
-  if (hasTypeScriptFile || !hasJavaScriptFile) {
-    return noJsConfig
-  }
-  return allowJsConfig
+    return allowJsConfig;
+    // let hasTypeScriptFile = false
+    // let hasJavaScriptFile = false
+    // let visitedFileCount = 0
+    // const visitPath = (directory: string): { stop: boolean } => {
+    //   if (directory.endsWith('.ts') || directory.endsWith('.tsx')) {
+    //     hasTypeScriptFile = true
+    //     return { stop: true }
+    //   }
+    //   if (directory.endsWith('.js') || directory.endsWith('.jsx')) {
+    //     hasJavaScriptFile = true
+    //   }
+    //   if (!fs.statSync(directory).isDirectory()) {
+    //     return { stop: false }
+    //   }
+    //   for (const child of fs.readdirSync(directory)) {
+    //     visitedFileCount++
+    //     if (visitedFileCount > maximumFileTraversalCount) {
+    //       return { stop: true }
+    //     }
+    //     const fullPath = path.resolve(directory, child)
+    //     const recursiveWalk = visitPath(fullPath)
+    //     if (recursiveWalk.stop) {
+    //       return recursiveWalk
+    //     }
+    //   }
+    //   return { stop: false }
+    // }
+    // visitPath(projectPath)
+    // if (hasTypeScriptFile || !hasJavaScriptFile) {
+    //   return noJsConfig
+    // }
+    // return allowJsConfig
 }
